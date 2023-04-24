bbd48109234bdd4b25dd76088388304e5c03d598
Edemilton Junior <edemiltonjr@gmail.com>
2023-04-24 20:11:27 -0300

    Navegação interna

--- src/screens/Login/index.js
+++ src/screens/Login/index.js
@@ -1,15 +1,26 @@
 import { Text, TouchableOpacity, View } from 'react-native'
 
-export default function Login(){
+export default function Login({navigation}){
     return(
         <View>
             <Text>Login</Text>
             {/* Inputs usuário e senha */}
-            <TouchableOpacity>
+            <TouchableOpacity
+                onPress={() => navigation.navigate('CreateUser')}
+            >
                 <Text>Cadastrar usuário</Text>
             </TouchableOpacity>
-            <TouchableOpacity>
+
+            <TouchableOpacity
+                onPress={() => navigation.navigate('CreateProfessional')}
+            >
                 <Text>Cadastrar profissional</Text>
+            </TouchableOpacity>
+
+            <TouchableOpacity
+                onPress={() => navigation.navigate('Tabs')}
+            >
+                <Text>Navegação</Text>
             </TouchableOpacity>
         </View>
     );
